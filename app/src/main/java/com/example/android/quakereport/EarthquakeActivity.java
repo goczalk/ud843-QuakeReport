9d2295ddfdf9884e0d3d1986c45a474a9e9aff17
Klaudia Goczal <klaudia@lodz.pl>
2018-08-01 21:38:21 +0200

    Added json parsing

--- app/src/main/java/com/example/android/quakereport/EarthquakeActivity.java
+++ app/src/main/java/com/example/android/quakereport/EarthquakeActivity.java
@@ -30,23 +30,7 @@ public class EarthquakeActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.earthquake_activity);
 
-        // Create a fake list of earthquake locations.
-
-//        ArrayList<String> earthquakes = new ArrayList<>();
-//        earthquakes.add("San Francisco");
-//        earthquakes.add("London");
-//        earthquakes.add("Tokyo");
-//        earthquakes.add("Mexico City");
-//        earthquakes.add("Moscow");
-//        earthquakes.add("Rio de Janeiro");
-//        earthquakes.add("Paris");
-
-        final ArrayList<Earthquake> earthquakes = new ArrayList<>();
-
-        earthquakes.add(new Earthquake(7.2, "San Francisco", "Feb 2, 2016"));
-        earthquakes.add(new Earthquake(3.3, "London", "Aug 2, 2015"));
-        earthquakes.add(new Earthquake(4.5, "Moscow", "Feb 22, 2014"));
-
+        final ArrayList<Earthquake> earthquakes = QueryUtils.extractEarthquakes();
 
         EarthquakeAdapter adapter = new EarthquakeAdapter(this, earthquakes);
 
